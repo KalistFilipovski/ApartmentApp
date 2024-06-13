ceb013a107f68b54c6fd115ed5210d85c277700a
Wirtinius <leekogreek@gmail.com>
2024-06-13 19:51:22 +0500

    feat: frontend implementation (correction) and requirements.txt added

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -5,8 +5,18 @@ db = SQLAlchemy()
 
 # User model
 class User(UserMixin, db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(250), unique=True, nullable=False)
-    password = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    full_name = db.Column(db.String(100), nullable=False)
+    bio = db.Column(db.Text)
+    profile_image = db.Column(db.String(100), default='default.jpg')
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
